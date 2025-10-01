6ea2eaba381fde772593a44817a4303e04b75ae0
7893 <etc@is20.net>
2025-10-01 13:30:21 +0800

    fix terraform format

--- infra/gcp/locals.tf
+++ infra/gcp/locals.tf
@@ -1,9 +1,9 @@
 locals {
   project_id = "sigma-outcome"
   region     = "us-central1"
-  
+
   runtime_service_account = "817261716888-compute@developer.gserviceaccount.com"
-  
+
   # 简化为2个函数
   functions = {
     dispatcher     = "dispatcher"
